3660a2bfe950a396a362fd0a34d2fde73d7f4f9e
Hawrami01 <hazhir.a.2004@gmail.com>
2025-01-01 04:42:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple JavaScript for extra functionality
+document.addEventListener('DOMContentLoaded', () => {
+    // Add event listeners or functions if needed, for example:
+    console.log("Resume website loaded");
+});
